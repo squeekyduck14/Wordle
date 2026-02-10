c843c45e79e67147dfe148fd6b4f9b00b5af8bb3
squeekyduck14 <ilemosstein@aimpa.org>
2026-02-10 09:43:55 -0500

    Update file?.py

--- file?.py
+++ file?.py
@@ -1,26 +1,30 @@
-
-# hard code approach - most direct
-
-
 from pathlib import Path
-word_file = Path("5-letter-word.txt")
 
-def file_word_file():
-    if word_file.exists():
-        return word_file
-    else:
-        print(" Error - File not found :( ")
-    
+# Change 1: need to include random library
+import random
 
-# Using a refernce document approach 
-def load_word_bank (filename = "5-letter-word.txt"):
-     with open(filename, "r") as file: 
-         return [line.strip().lower() for line in file if len(line.strip()) == 5]
- 
-         
-word_file = load_word_bank()
+word_file = Path("5-letter-words.txt")
 
+def find_word_file():
+	if word_file.exists():
+		return word_file
+	else:
+		print(" Error - File not found! :( ")
+
+# Using a reference document approach
+def load_word_bank (filename = "5-letter-words.txt"):
+	with open(filename, "r") as file:
+		return [line.strip().lower() for line in file if len(line.strip()) == 5]
+	
+word_bank = load_word_bank()
 
 def main():
-    print("Wordle Test Project")
-    # using the word_file call on a random element in the file
+	print("Wordle Test Project")
+	# Change 2: 
+	words = load_word_bank(word_file)
+	sample  = random.choice(words)
+	print(f"Test Sample Word: {sample}")
+
+# Change 3: Need to run main function, so that is written below
+if __name__ == "__main__":
+	main()
